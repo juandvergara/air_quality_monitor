acc2984c93815bc403d02fc7a8624e87b0a4797c
Juan David Vergara <jdvergaram.dev@gmail.com>
2025-06-05 22:33:39 -0500

    Add method to get multiple info sensors

--- purple_client/client.py
+++ purple_client/client.py
@@ -11,17 +11,30 @@ class PurpleAirClient:
 
     def __init__(self):
         self.api_key = os.getenv("PURPLEAIR_API_KEY")
+        self.headers = {"X-API-Key": self.api_key}
         if not self.api_key:
             raise ValueError("API Key no encontrada en variables de entorno")
-
-    def get_sensor(self, sensor_id: int):
-        url = f"{self.BASE_URL}/sensors/{sensor_id}"
-        headers = {"X-API-Key": self.api_key}
-        response = requests.get(url, headers=headers)
-        data = response.json().get("sensor", {})
-
+    
+    def _handle_response(self, response):
         if response.status_code != 200:
             raise Exception(f"Error en la API: {response.status_code} - {response.text}")
+        return response.json()
+    
+    def get_sensor(self, sensor_id: int):
+        """
+        Obtiene los datos de un sensor específico por su ID.
+        
+        Args: 
+            :param sensor_id: ID del sensor a consultar.
+        
+        Returns:
+            Un objeto SensorData con la información del sensor.
+        """
+        url = f"{self.BASE_URL}/sensors/{sensor_id}"
+        response = requests.get(url, headers=self.headers)
+        data = response.json().get("sensor", {})
+
+        self._handle_response(response)
 
         return SensorData(
             sensor_id=data.get("sensor_index"),
@@ -35,3 +48,43 @@ class PurpleAirClient:
             confidence=data.get("confidence"),
             last_seen=data.get("last_seen")  
         )
+    
+    def get_sensors(self, bounds: dict):
+        """
+        Obtiene una lista de sensores dentro de los límites especificados.
+        :param bounds: Un diccionario con las coordenadas de los límites:
+            {
+                "nwlng": float,
+                "nwlat": float,
+                "selng": float,
+                "selat": float
+            }
+        :return: Una lista de objetos SensorData con la información de los sensores.
+        
+        """
+        url = f"{self.BASE_URL}/sensors"
+        params = {
+            'fields': 'sensor_index,name,latitude,longitude,temperature,humidity,pressure,pm2.5,confidence,last_seen',
+            "nwlng": bounds["nwlng"],
+            "nwlat": bounds["nwlat"],
+            "selng": bounds["selng"],
+            "selat": bounds["selat"]
+        }
+        fields = params["fields"].split(',')
+        response = requests.get(url, headers=self.headers, params=params)
+        data = response.json().get("data", [])
+
+        self._handle_response(response)
+
+        return [SensorData(
+            sensor_id=sensor[fields.index("sensor_index")],
+            name=sensor[fields.index("name")],
+            latitude=sensor[fields.index("latitude")],
+            longitude=sensor[fields.index("longitude")],
+            temperature=sensor[fields.index("temperature")],
+            humidity=sensor[fields.index("humidity")],
+            preassure=sensor[fields.index("pressure")],
+            pm2_5=sensor[fields.index("pm2.5")],
+            confidence=sensor[fields.index("confidence")],
+            last_seen=sensor[fields.index("last_seen")]
+        ) for sensor in data]
