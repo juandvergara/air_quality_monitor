ec1abb5b41b9e6c56cae73cddc41f862be9b09fb
Juan David Vergara <jdvergaram.dev@gmail.com>
2025-06-07 22:43:11 -0500

    Add SensorData class and reuse the AQI calculate

--- purple_client/models.py
+++ purple_client/models.py
@@ -2,6 +2,35 @@ from dataclasses import dataclass
 from typing import Optional
 
 
+def _AQI_from_pm2_5(pm2_5: float) -> int:
+        """
+        Determina el AQI basado en los rangos de PM2.5.
+        """
+        if pm2_5 is None:
+            return -1
+        if pm2_5 > 350.5:
+            return _calculate_AQI(pm2_5, 500, 401, 500.4, 350.5)  # Hazardous
+        elif pm2_5 > 250.5:
+            return _calculate_AQI(pm2_5, 400, 301, 350.4, 250.5)  # Hazardous
+        elif pm2_5 > 150.5:
+            return _calculate_AQI(pm2_5, 300, 201, 250.4, 150.5)  # Very Unhealthy
+        elif pm2_5 > 55.5:
+            return _calculate_AQI(pm2_5, 200, 151, 150.4, 55.5)  # Unhealthy
+        elif pm2_5 > 35.5:
+            return _calculate_AQI(pm2_5, 150, 101, 55.4, 35.5)  # Unhealthy for Sensitive Groups
+        elif pm2_5 > 12.1:
+            return _calculate_AQI(pm2_5, 100, 51, 35.4, 12.1)  # Moderate
+        elif pm2_5 >= 0:
+            return _calculate_AQI(pm2_5, 50, 0, 12, 0)  # Good
+        else:
+            return -1
+
+def _calculate_AQI(Cp: float, Ih: int, Il: int, BPh: float, BPl: float) -> int:
+        a = Ih - Il
+        b = BPh - BPl
+        c = Cp - BPl
+        return round((a / b) * c + Il)
+
 @dataclass
 class SensorData:
     sensor_id: int
@@ -17,36 +46,7 @@ class SensorData:
     aqi: int = None
 
     def __post_init__(self):
-        self.aqi = self._AQI_from_pm2_5(self.pm2_5)
-
-    def _AQI_from_pm2_5(self, pm2_5: float) -> int:
-        """
-        Determina el AQI basado en los rangos de PM2.5.
-        """
-        if pm2_5 is None:
-            return -1
-        if pm2_5 > 350.5:
-            return self._calculate_AQI(pm2_5, 500, 401, 500.4, 350.5)  # Hazardous
-        elif pm2_5 > 250.5:
-            return self._calculate_AQI(pm2_5, 400, 301, 350.4, 250.5)  # Hazardous
-        elif pm2_5 > 150.5:
-            return self._calculate_AQI(pm2_5, 300, 201, 250.4, 150.5)  # Very Unhealthy
-        elif pm2_5 > 55.5:
-            return self._calculate_AQI(pm2_5, 200, 151, 150.4, 55.5)  # Unhealthy
-        elif pm2_5 > 35.5:
-            return self._calculate_AQI(pm2_5, 150, 101, 55.4, 35.5)  # Unhealthy for Sensitive Groups
-        elif pm2_5 > 12.1:
-            return self._calculate_AQI(pm2_5, 100, 51, 35.4, 12.1)  # Moderate
-        elif pm2_5 >= 0:
-            return self._calculate_AQI(pm2_5, 50, 0, 12, 0)  # Good
-        else:
-            return -1
-
-    def _calculate_AQI(self, Cp: float, Ih: int, Il: int, BPh: float, BPl: float) -> int:
-        a = Ih - Il
-        b = BPh - BPl
-        c = Cp - BPl
-        return round((a / b) * c + Il)
+        self.aqi = _AQI_from_pm2_5(self.pm2_5)
     
     def to_dict(self):
         return {
@@ -62,3 +62,16 @@ class SensorData:
             "last_seen": self.last_seen,
             "aqi": self.aqi
         }
+
+
+@dataclass
+class SensorHistory:
+    sensor_id: int
+    timestamp: int
+    pm2_5: Optional[float]
+    temperature: Optional[int]
+    humidity: Optional[int]
+    aqi: int = None
+
+    def __post_init__(self):
+        self.aqi = _AQI_from_pm2_5(self.pm2_5)
